3c7246f85d7fc755683049b565c4c06c5635971d
hp1909 <hoangphuc1909@live.com>
2021-07-03 16:43:06 +0700

    Initial Commit

---
+++ MovieCollections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieCollections
+//
+//  Created by Phuc Hoang on 7/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
